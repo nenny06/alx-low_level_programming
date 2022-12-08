672bd40ba256b17a876518cf0097997cdac057f5
Nnenna <nnennuzo@gmail.com>
2022-12-08 13:23:29 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
+
+
+
+
 
 
 
