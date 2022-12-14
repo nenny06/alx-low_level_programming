8d1d4e02695a4d6b7ee91ab0b60885ed12cecda1
Nnenna <nnennuzo@gmail.com>
2022-12-14 04:49:12 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,18 +6,15 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int x;
 
 	if (n < 0)
-	{
-		last_digit = (-1 * (n % 10));
-		_putchar(last_digit * "0");
-		return (last_digit);
-	}
-	else
-	{
-		last_digit = (n % 10);
-		-putchar(last_digit * '0');
-		return (last_digit);
-	}
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
+
+
+
+
