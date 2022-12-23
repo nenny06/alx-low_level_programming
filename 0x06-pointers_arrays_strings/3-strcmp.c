7495536801d8573db29f0c33fe48f987fbf0c8ac
Nnenna <nnennauzo55@gmail.com>
2022-12-23 14:20:59 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,31 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i 0, diff = 0;
-
-	while (1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-				break;
-
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = s1[i] - s2[i];
-			break;
-		}
-		else
-			i++;
+		s1++;
+		s2++;
 	}
-	return (diff);
+	return (*s1 - *s2);
 }
 
