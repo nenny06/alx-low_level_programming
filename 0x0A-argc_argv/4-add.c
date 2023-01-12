2d11d64c239710a7dfecd485321d834b71da24da
Nnenna <nnennauzo55@gmail.com>
2023-01-11 19:43:32 -0800

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * isNumeric - checks if a string is a number or not
+ * @str: string to be checked
+ * Return: true if number, otherwise false
+ */
+int isNumeric(const char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: array size
+ * @argv: array containing elements
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int count = 1, sum = 0;
+
+	while (count < argc)
+	{
+		if (isNumeric(argv[count]) == 0)
+		{
+			puts("Error");
+			return (1);
+		}
+		sum = sum = atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
