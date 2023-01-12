a91c730746b60d3104090f0a8181cebe01905dd6
Nnenna <nnennauzo55@gmail.com>
2023-01-12 15:49:29 -0800

    all tasks

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @neeedle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
