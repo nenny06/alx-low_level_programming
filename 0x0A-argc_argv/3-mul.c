bf6bdfc25112b19a6b013e84ceaae16fc6ce1eeb
Nnenna <nnennauzo55@gmail.com>
2023-01-11 19:22:45 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies 2 numbers
+ * @argc: array size
+ * @argv: array containing elements
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int arr_len = argc - 1;
+
+	if (arr_len != 2)
+	{
+		puts("Error");
+
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
