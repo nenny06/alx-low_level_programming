f821abdd92d512b9ab3576ea297fff33ca08f9fe
Nnenna <nnennauzo55@gmail.com>
2023-01-17 13:19:54 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - fills memory withanother buffer
+ * @dest: source string
+ * @src: string for filing
+ * @n: length of buffer
+ * Return: new string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int = 0;
+
+	while (i < 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
